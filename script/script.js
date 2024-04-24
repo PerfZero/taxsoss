07d90b416e80ef69ac03f275a1b472326d5f9372
Denis <gundneis1@gmail.com>
2024-04-24 13:33:00 +0300

    Main

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.income__slider', {
+        slidesPerView: 3,
+        spaceBetween: 50,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
